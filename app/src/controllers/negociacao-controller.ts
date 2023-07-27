24fff965dff1f5b84ed0426323a60d1c08810a80
Adriano Santos <adriano_angelo@live.com>
2023-07-27 12:47:23 -0300

    API do Decorator

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -20,7 +20,7 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    @logarTempoDeExecucao
+    @logarTempoDeExecucao()
     public adiciona(): void {
         /*
             Zé, você já viu isso?
