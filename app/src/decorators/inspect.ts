af6a7854db08fb1df9ff1c3470c967450fcc0e10
Adriano Santos <adriano_angelo@live.com>
2023-07-28 12:41:53 -0300

    Simplificando o Decorator

--- app/src/decorators/inspect.ts
+++ app/src/decorators/inspect.ts
@@ -1,18 +1,16 @@
-export function inspect() {
-    return function (
+export function inspect (
         target: any,
         propertyKey: string,
         descriptor: PropertyDescriptor
-    ) {
-        const metodoOriginal = descriptor.value;
-        descriptor.value = function(...args: any[]) {
-            console.log(`___Método: ${propertyKey}`)
-            console.log(`___parâmetros: ${JSON.stringify(args)}`)
-            const retorno = metodoOriginal.apply(this, args);
-            console.log(`___retorno: ${JSON.stringify(retorno)}`)
-            
-            return retorno;
-        }
-        return descriptor;
+) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function(...args: any[]) {
+        console.log(`___Método: ${propertyKey}`)
+        console.log(`___parâmetros: ${JSON.stringify(args)}`)
+        const retorno = metodoOriginal.apply(this, args);
+        console.log(`___retorno: ${JSON.stringify(retorno)}`)
+        
+        return retorno;
     }
+    return descriptor;
 }
