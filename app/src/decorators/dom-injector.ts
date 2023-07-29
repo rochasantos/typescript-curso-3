7ff99afd275dc82435cfcd35784259efcc354992
Adriano Santos <adriano_angelo@live.com>
2023-07-29 10:16:50 -0300

    Aplicando cache decorator copy

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,10 +1,16 @@
 export function domInjector(seletor: string) {
     return function ( target: any, propertyKey: string ) {
         console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para  a propriedade ${propertyKey}`);
+
+        let elemento: HTMLElement;
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
-            return elemento;
+            
+            if (!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+            }           
+            
+            return elemento;            
         }
 
         Object.defineProperty(
