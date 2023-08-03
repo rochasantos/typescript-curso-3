7f6fad57315cb47e9aabbe24e8fd9de0442a42c7
Adriano Santos <adriano_angelo@live.com>
2023-08-03 20:28:39 -0300

    Interface com Generics

--- app/dist/js/models/negociacoes.js
+++ app/dist/js/models/negociacoes.js
@@ -11,4 +11,7 @@ export class Negociacoes {
     paraTexto() {
         return JSON.stringify(this.negociacoes, null, 2);
     }
+    ehIgual(objeto) {
+        return JSON.stringify(this.negociacoes) === JSON.stringify(objeto.lista());
+    }
 }
