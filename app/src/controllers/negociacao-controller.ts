b4180917075ce4df7c299a7bfd748e70e3f7f99c
Adriano Santos <adriano_angelo@live.com>
2023-08-03 09:23:59 -0300

    Consumindo uma API

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -47,7 +47,24 @@ export class NegociacaoController {
     }
 
     importaDados(): void {
-        alert('oi');
+        fetch('http://localhost:8080/dados')
+            .then(res => res.json())
+            .then((dados: any[]) => {
+                return dados.map(dadosDeHoje => {
+                    return new Negociacao(
+                        new Date(),
+                        dadosDeHoje.vezes,
+                        dadosDeHoje.montante
+                    );
+                })
+            })
+            .then(negociacoesDeHoje => {
+                for(let negociacao of negociacoesDeHoje) {
+                    this.negociacoes.adiciona(negociacao)
+                }
+                this.negociacoesView.update(this.negociacoes)
+            })
+
     }
 
     private ehDiaUtil(data: Date) {
